30ea6105f4b590ce9acab0653ebd4781386bc687
Konstantinn56 <koceproto@gmail.com>
2022-03-05 13:02:07 +0200

    .

--- MovieLibrary/MLibUI/LoginPage.Designer.cs
+++ MovieLibrary/MLibUI/LoginPage.Designer.cs
@@ -268,7 +268,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(31, 0);
+            this.label2.Location = new System.Drawing.Point(29, 1);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 16);
             this.label2.TabIndex = 1;
