857aabc8df8eebd9bd905af36171986e33795050
Velina666 <velina_yordanova004@abv.bg>
2022-03-05 12:46:07 +0200

    Film

---
+++ MovieLibrary/Data/Model/Film.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Film Entity
+/// </summary>
+namespace Data.Model
+{
+    public class Film
+    {
+        /// <summary>
+        /// The id of the person who add a film
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the added film
+        /// </summary>
+        public int Title { get; set; }
+
+        /// <summary>
+        /// The publisher of the film
+        /// </summary>
+        public int Publisher { get; set; }
+
+        /// <summary>
+        /// The year the film comes out
+        /// </summary>
+        public int FilmYaer { get; set; }
+
+        /// <summary>
+        /// The type of the film
+        /// </summary>
+        public int FilmType { get; set; }
+
+        /// <summary>
+        /// User evaluation for the film
+        /// </summary>
+        public int Stars { get; set; }
+
+        /// <summary>
+        /// The rating of the film 
+        /// </summary>
+        public int Rate { get; set; }
+    }
+}
