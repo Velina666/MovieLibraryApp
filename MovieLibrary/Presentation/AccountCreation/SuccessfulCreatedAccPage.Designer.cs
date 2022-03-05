23a1f5129152af794cb9162136950a1bd4bb04c4
Konstantinn56 <koceproto@gmail.com>
2022-03-05 09:15:20 +0200

    .

---
+++ MovieLibrary/Presentation/AccountCreation/SuccessfulCreatedAccPage.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Presentation.AccountCreation
+{
+    partial class SuccessfulCreatedAccPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SuccessfulCreatedAccPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 337);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SuccessfulCreatedAccPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SuccessfulCreatedAccPage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
